0738f4e2e891efec3373d78244338b9325d3cc02
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-06-26 14:40:57 -0700

    Add React integration, fix codeblock theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()]
+  integrations: [tailwind(), icon(), mdx(), react()],
+  markdown: {
+    shikiConfig: {
+      theme: "github-light"
+    }
+  }
 });
