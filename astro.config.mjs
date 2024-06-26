72b044098e4dce8c5255a0614f0c6428e2d99d1d
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-06-26 12:03:02 -0700

    Add MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), mdx()]
 });
