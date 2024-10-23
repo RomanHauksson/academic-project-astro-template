303222e22821eb417231adba9363335ba7286f35
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-10-23 10:28:30 -0500

    Check whether base URL ends with a slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: "never",
   integrations: [tailwind(), icon(), mdx(), react()],
   markdown: {
     shikiConfig: {
