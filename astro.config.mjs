ea42326931a5b2f1f69861a9b00653487c14e1de
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2025-10-14 13:55:27 -0500

    Add font weights

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,8 @@ export default defineConfig({
     fonts: [{
       provider: fontProviders.google(),
       name: "Noto Sans",
-      cssVariable: "--font-noto-sans"
+      cssVariable: "--font-noto-sans",
+      weights: [100, 200, 300, 400, 500, 600, 700, 800, 900]
     }]
   }
 });
