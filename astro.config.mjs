6a2c4f5f21fc56c4b99ee5fdbd0df5d90945ded3
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2025-10-17 00:01:46 -0500

    Change font weight range to use variable font syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
       provider: fontProviders.google(),
       name: "Noto Sans",
       cssVariable: "--font-noto-sans",
-      weights: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+      weights: "100 900",
     }]
   }
 });
