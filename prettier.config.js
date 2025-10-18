fff86e9ab1a20ac541adcd1c1b2e287510ad334f
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2025-10-18 17:10:51 -0500

    Use default prettier settings

--- prettier.config.js
+++ prettier.config.js
@@ -3,10 +3,6 @@
  * @type {import("prettier").Config & import('prettier-plugin-tailwindcss').PluginOptions}
  */
 const config = {
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
     plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
     overrides: [{ files: '*.astro', options: { parser: 'astro' } }],
 }
