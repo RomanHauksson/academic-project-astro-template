b9c777cb52181ecab2acee240d5268980fa0411f
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2025-10-23 12:33:24 -0500

    Use Javascript Prettier config file

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,4 @@
-import { type Config } from "prettier";
-
-const config: Config = {
+const config = {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [{ files: "*.astro", options: { parser: "astro" } }],
 };
