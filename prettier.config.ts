935c53653b26a71855b070cbc2460517c96bf5be
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2025-11-29 00:55:41 -0800

    Bring back Typescript prettier config

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,4 +1,6 @@
-const config = {
+import { type Config } from "prettier";
+
+const config: Config = {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [{ files: "*.astro", options: { parser: "astro" } }],
 };
